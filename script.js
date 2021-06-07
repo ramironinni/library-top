71cc5a0148a7f6ec714964aec9c16323e8f356d0
ramironinni <ramironinni@gmail.com>
2021-06-06 21:13:43 -0300

    Add styles and create display

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 window.addEventListener("DOMContentLoaded", () => {
-    const myLibrary = [];
+    const myLibrary = [
+        {
+            title: "A Study in Scarlet",
+            author: "Sir Arthur Ignatius Conan Doyle",
+            pages: 221,
+            read: true,
+        },
+        {
+            title: "The Call of Cthulhu",
+            author: "H. P. Lovecraft",
+            pages: 67,
+            read: true,
+        },
+        {
+            title: "The Big Four",
+            author: "Agatha Christie",
+            pages: 125,
+            read: false,
+        },
+    ];
 
     function Book(title, author, pages, read) {
         this.title = title;
@@ -34,5 +53,29 @@ window.addEventListener("DOMContentLoaded", () => {
         console.log(myLibrary);
     }
 
-    function displayBooks() {}
+    function displayBooks() {
+        const booksTilesContainer = document.getElementById(
+            "books-tiles-container "
+        );
+        myLibrary.forEach((book) => {
+            const bookTile = document.createElement("div");
+            bookTile.classList.add("book-tile");
+
+            const title = document.createElement("p");
+            title.innerText = book.title;
+            bookTile.appendChild(title);
+
+            const author = document.createElement("p");
+            author.innerText = book.author;
+            bookTile.appendChild(author);
+
+            const pages = document.createElement("p");
+            pages.innerText = `${book.pages} pages.`;
+            bookTile.appendChild(pages);
+
+            booksTilesContainer.appendChild(bookTile);
+        });
+    }
+
+    displayBooks();
 });
